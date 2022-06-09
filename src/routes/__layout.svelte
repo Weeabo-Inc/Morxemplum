b91cea316756dfdbc9864285bfff7783876c4017
Edge <ilarumbgim1@educacion.navarra.es>
2022-06-09 21:56:14 +0200

    feat: typography and about me page

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,5 @@
 <script>
     import "./../app.css"
 </script>
-<div class="bg-zinc-800 p-4 text-4xl font-bold text-gray-300">Morxemplum</div>
+<a href="/"><div class="bg-zinc-900  p-4 text-4xl font-bold text-gray-300 hover:text-white transition-all">Morxemplum</div></a>
 <slot />
---
+++ src/routes/about/index.svelte
@@ -0,0 +1,45 @@
+<div class="mt-12 container mx-auto w-3/4">
+	<div class="flex">
+		<div class="flex-1">
+			<div
+				class="text-[70px] mx-auto w-max font-bold bg-clip-text text-transparent bg-gradient-to-r from-rose-500 via-indigo-500 to-blue-500"
+			>
+				I'm GameWebcam
+			</div>
+			<div class="prose xl:prose-xl mx-auto invert text-justify">
+				<h3>Content Creator and Gamer</h3>
+				<p>
+					Content creator of sandbox, let's plays, open source culture, commentary, and some memes
+					for the fun of it. Don't mind the channel name; I came up with it when I was 7. I'm a
+					he/him, just so you don't get it confused.
+				</p>
+				<p>
+					This is my first YouTube channel I created, before creating Metrono Ivernan. Now that I've
+					retired Metrono Ivernan, I'm back and not going anywhere. The primary goal of this channel
+					is to make the content that I enjoy making, regardless of what type of content it is and
+					not suffer the same fate Metrono Ivernan did. I do have plans on making visual arts and
+					film, but those are ambitions at the moment.
+				</p>
+			</div>
+		</div>
+		<div class="w-96  mr-48">
+			<img src="/images/pfp.png" class="rounded-xl shadow-lg" alt="Morxemplum Logo" />
+			<figure class="text-center text-white font-thin italic mt-2">Morxemplum Logo</figure>
+			<a href="https://www.youtube.com/user/gamewebcam/"><div class="p-2 text-center font-extrabold text-lg bg-rose-800 text-white mt-2 rounded">YouTube</div></a>
+			<a href="https://discord.com/invite/DTA9NcTkAp"
+				><div class="p-2 rounded text-center font-extrabold text-lg bg-indigo-800 text-white mt-2">
+					Discord
+				</div></a>
+		</div>
+	</div>
+	<div>
+		<iframe
+			class=" mt-6 lg:w-3/4 w-full h-96 mx-auto lg:h-[720px]"
+			src="https://www.youtube-nocookie.com/embed/EqSWN_sbx6A"
+			title="YouTube video player"
+			frameborder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+			allowfullscreen
+		/>
+	</div>
+</div>
